9f85634b7dd1e4280bdd95a9b36e80f92e01841f
WW <94125132+William-Wang-Dev@users.noreply.github.com>
2022-08-24 00:52:18 +0800

    Add files via upload
    fix misspelling

--- src/authorisation/UnitTest/authorisationUnitTest.py
+++ src/authorisation/UnitTest/authorisationUnitTest.py
@@ -16,7 +16,7 @@ class TestAuthorisation(unittest.TestCase):
         AuthorisationDataBaseMock.get_deleting.return_value = user_list
         AuthorisationDataBaseMock.get_uploading.return_value = user_list
 
-    def test_has_premission_true(self):
+    def test_has_permission_true(self):
         sessionManagerMock = Mock()
         sessionManagerMock.get.return_value = namedtuple("UserInfo", "name is_admin, login_time", defaults=["test", False, time.time()])()
         AuthorisationDataBaseMock = Mock()
@@ -24,13 +24,13 @@ class TestAuthorisation(unittest.TestCase):
         self._init_data(user_list, AuthorisationDataBaseMock)
 
         auth = Authorisation(sessionManagerMock, AuthorisationDataBaseMock)
-        self.assertTrue(auth.has_premission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
 
-    def test_has_premission_false(self):
+    def test_has_permission_false(self):
         sessionManagerMock = Mock()
         sessionManagerMock.get.return_value = namedtuple("UserInfo", "name is_admin, login_time", defaults=["test", False, time.time()])()
         AuthorisationDataBaseMock = Mock()
@@ -38,13 +38,13 @@ class TestAuthorisation(unittest.TestCase):
         self._init_data(user_list, AuthorisationDataBaseMock)
 
         auth = Authorisation(sessionManagerMock, AuthorisationDataBaseMock)
-        self.assertFalse(auth.has_premission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
 
-    def test_has_premission_without_login(self):
+    def test_has_permission_without_login(self):
         sessionManagerMock = Mock()
         sessionManagerMock.get.return_value = None
         AuthorisationDataBaseMock = Mock()
@@ -52,13 +52,13 @@ class TestAuthorisation(unittest.TestCase):
         self._init_data(user_list, AuthorisationDataBaseMock)
 
         auth = Authorisation(sessionManagerMock, AuthorisationDataBaseMock)
-        self.assertFalse(auth.has_premission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertFalse(auth.has_premission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertFalse(auth.has_permission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
 
-    def test_has_premission_admin(self):
+    def test_has_permission_admin(self):
         sessionManagerMock = Mock()
         sessionManagerMock.get.return_value = namedtuple("UserInfo", "name is_admin, login_time", defaults=["test", True, time.time()])()
         AuthorisationDataBaseMock = Mock()
@@ -66,11 +66,11 @@ class TestAuthorisation(unittest.TestCase):
         self._init_data(user_list, AuthorisationDataBaseMock)
 
         auth = Authorisation(sessionManagerMock, AuthorisationDataBaseMock)
-        self.assertTrue(auth.has_premission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
-        self.assertTrue(auth.has_premission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.READING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.WRITING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.DOWNLOADING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.DELETING, file=os.path.dirname(__file__)+r"/testFile.txt"))
+        self.assertTrue(auth.has_permission("test", AuthorisationType.UPLOADING, file=os.path.dirname(__file__)))
 
 
 if __name__ == "__main__":
