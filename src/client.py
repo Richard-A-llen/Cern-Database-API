33f88d10debc72808013bad8d7f5fbff368b5e7a
WW <94125132+William-Wang-Dev@users.noreply.github.com>
2022-08-28 23:33:42 +0800

    Update client.py
    remove useless code and prepare for testing

--- src/client.py
+++ src/client.py
@@ -1,63 +1,53 @@
-import random
 import os
 import shutil
 import requests
-import ssl
 
 current_dir = os.path.dirname(__file__)
 server_crt_file = os.path.dirname(__file__) + r'./flask_cert.pem'
+request_successful = 200
 
-def test_index():
-    response = requests.get(r'http://127.0.0.1:8080')
+
+def login(user, passwd):
+    response = requests.get(
+        f'https://127.0.0.1:8080/request/login?user={name}&passwd={passwd}', verify=server_crt_file)
     print(f'response = {response}')
     print(f'response.content = {response.content}')
+    if response.status_code == request_successful:
+        return response.content # receive session
+    print("login failed.")
+    return None
 
-
-def task():
-    sleep = random.randint(5, 10)
-    response = requests.get(f'http://127.0.0.1:8080/task/do_some?name=test&taks=eating')
-    print(f'response = {response}')
-    print(f'response.content = {response.content}')
-
-
-def upload_file():
-    # you can change file and save_name to test
-    # save_name used to save file with the name on the remote server side
-    file = r"D:\MyPrograms\MicroserviceWithFlask\client\A Man Without Love.mp3"
-    if (not os.path.exists(file)):
-        print("the file doesn't exist.")
-        return
-    save_name = r"demo.mp3"
-    url = f'http://127.0.0.1:8080/upload/upload?user=test&file_name={save_name}'
-    response = requests.post(url, files={'file': open(file, 'rb')})
-    print(f'response = {response}')
-    print(f'response.content = {response.content}')
-
-
-def download_file():
-    with requests.get(f'https://127.0.0.1:8080/request/download?name=test&file=test', stream=True, verify=server_crt_file) as f:
+def download(session, file):
+    with requests.get(f'https://127.0.0.1:8080/request/download?session={session}&file=test', stream=True, verify=server_crt_file) as f:
+        if f.status_code != request_successful:
+            print(f"{f.content}")
+            return
         with open(r'D:\MyPrograms\MicroserviceWithFlask\misc\test', 'wb') as w:
             shutil.copyfileobj(f.raw, w)
-    print(f'response = {f.status_code}')
+            print(r"download successful")
 
+def upload(session, local_file):
+    # you can change file and save_name to test
+    # save_name used to save file with the name on the remote server side
+    if not (os.path.exists(local_file) and os.path.isfile(local_file)):
+        print("the file doesn't exist.")
+        return
+    file_name = os.path.basename(local_file)
+    url = f'https://127.0.0.1:8080/upload/upload?session={session}&file_name={file_name}'
+    response = requests.post(url, files={'file': open(local_file, 'rb')})
+    if response.status_code == request_successful:
+        print("upload successful")
+    else:
+        print("upload failed.")
+    print(f"{response.content}")
 
-def login(name, passwd):
-    response = requests.get(f'http://127.0.0.1:8080/login/?user={name}&passwd={passwd}')
-    print(f'response = {response}')
-    print(f'response.content = {response.content}')
-
-
-def task_login(name, passwd):
-    response = requests.get(f'https://127.0.0.1:8080/request/login?user={name}&passwd={passwd}', verify=server_crt_file)
-    print(f'response = {response}')
-    print(f'response.content = {response.content}')
+def user_demo():
+    user = "test"
+    password = "4f@d64E0c!0"
+    session = login(user, password)
+    download(session, "test.txt")
+    upload(session, "local_file.txt")
 
 
 if __name__ == '__main__':
-    # task_login("test", 3333)
-    download_file()
-    # login("test", 1234)
-    # test_index()
-    # task()
-    # upload_file()
-    # download_file()
+    user_demo()
