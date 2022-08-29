9bfc5fe237a9addfe0afe7b824b9218b5dcd82ea
grzegorzpikus <grzegorz.pikus@gmail.com>
2022-08-29 14:17:58 +0100

    Unittests have been added. Small corrections were applied in the code.

--- src/Authentication/Authentication.py
+++ src/Authentication/Authentication.py
@@ -16,8 +16,6 @@ def create_user_object(user_name):
     If it is 0 (int), the function creates User object.
     """
 
-    # conn = sqlite3.connect('C:/Users/grzeg/PycharmProjects/Authentication/'
-    #                        'database_file.db')
     conn = sqlite3.connect('database_file.db')
     c = conn.cursor()
     c.execute("""SELECT * FROM users;""")
@@ -26,16 +24,13 @@ def create_user_object(user_name):
         if row[2] == user_name:
             # The user has an administrator status.
             if row[4] == 1:
-                user_object = Cls.Administrator(row[2], row[3], row[0],
-                                                row[1], row[4])
+                user_object = Cls.Administrator(row[0], row[1], row[2],
+                                                row[3], row[4])
                 return user_object
             # The user does not have an administrator status.
-            else:
-                user_object = Cls.User(row[2], row[3], row[0],
-                                                row[1], row[4])
+            elif row[4] == 0:
+                user_object = Cls.User(row[0], row[1], row[2],
+                                                row[3], row[4])
                 return user_object
-
-
-
-
-
+            else:
+                return None
